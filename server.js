ef54401440c6f3d9b500cda314a15a844d4b5fd2
Ben Cooper <ben.f.cooper@gmail.com>
2021-06-02 14:51:23 +0100

    Updated server file to include the correct listening port.

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const app = express();
 app.engine('pug', require('pug').__express);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
-app.get('/', (req, res)=>{res.render('index', {
-    title: 'Home'
-})});
-app.listen(process.env.PORT || 8080, ()=>{console.log("Express server listening on port %d in %s mode",
-    "7", app.settings.env)});
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Home'
+    })
+});
+app.listen(process.env.PORT || 8080, function () {
+    console.log("Express server listening on port %d in %s mode",
+        this.address().port, app.settings.env)
+});

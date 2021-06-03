8cac10030d3f0d5550375161dd831395bfbf0a48
Thiruisa <45204969+Thiruisa@users.noreply.github.com>
2021-06-03 13:48:31 +0100

    Updated Index

--- server.js
+++ server.js
@@ -3,10 +3,15 @@
 const express = require('express');
 const app = express();
 app.engine('pug', require('pug').__express);
+app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
-app.get('/', (req, res)=>{res.render('index', {
-    title: 'Home'
-})});
-app.listen(process.env.PORT || 8080, ()=>{console.log("Express server listening on port %d in %s mode",
-    "7", app.settings.env)});
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Home'
+    })
+});
+app.listen(process.env.PORT || 8080, function () {
+    console.log("Express server listening on port %d in %s mode",
+        this.address().port, app.settings.env)
+});

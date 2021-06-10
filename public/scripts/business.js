39e5ac14fefe442210f94083509176c532d3c58d
Abena Serwaa Johene Amo <ecn19seu@uea.ac.uk>
2021-06-10 22:35:32 +0000

    Completed references for the cycling

--- public/scripts/business.js
+++ public/scripts/business.js
@@ -1,4 +1,4 @@
-//Get the hotels modal id
+//Enable modals for the references to open and close.
 
 document.getElementById("modalbutton").addEventListener(
     "click",()=>{
@@ -8,5 +8,16 @@ document.getElementById("modalbutton").addEventListener(
 document.getElementById("closeid").addEventListener(
     "click",()=>{
         document.getElementById("kitemodal").className = "modal"
+    }
+)
+
+document.getElementById("cyclebutton").addEventListener(
+    "click",()=>{
+        document.getElementById("cyclemodal").className = "modal active"
+    }
+)
+document.getElementById("closecycle").addEventListener(
+    "click",()=>{
+        document.getElementById("cyclemodal").className = "modal"
     }
 )

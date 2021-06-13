e122cdf908a1ba274064c2bfb6234f0e020baee2
Thiruisa <45204969+Thiruisa@users.noreply.github.com>
2021-06-13 12:37:44 +0100

    Changed tabs for local business

---
+++ public/scripts/localBusiness.js
@@ -0,0 +1,31 @@
+if(window.location.pathname === "/upload"){
+    let tabs = document.getElementsByClassName("tab-item");
+    for(let i = 0; i<tabs.length; i++){
+        if(tabs[i].className === "tab-item active"){
+            if(tabs[i].children[0].innerText === "Subir"){
+                tabs[i].children[0].innerText = "👩🏽‍💼 Subir"
+
+            }
+        }
+    }
+}
+else if(window.location.pathname === "/business"){
+    let tabs = document.getElementsByClassName("tab-item");
+    for(let i = 0; i<tabs.length; i++){
+        if(tabs[i].className === "tab-item active"){
+            if(tabs[i].children[0].innerText === "Negocios"){
+                tabs[i].children[0].innerText = "🏪 Negocios"
+            }
+        }
+    }
+}
+else if(window.location.pathname === "/explore"){
+    let tabs = document.getElementsByClassName("tab-item");
+    for(let i = 0; i<tabs.length; i++){
+        if(tabs[i].className === "tab-item active"){
+            if(tabs[i].children[0].innerText === "Explore Nuevos Mercados"){
+                tabs[i].children[0].innerText = "🌎 Explore Nuevos Mercados"
+            }
+        }
+    }
+}
